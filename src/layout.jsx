b19240d3f4157cc80cf8036c46a854666909b10d
Sebastian Aage Nielsen <u33674@rts-365.dk>
2023-10-31 06:10:00 +0100

    removed geist as standard font and switched back to default

--- src/layout.jsx
+++ src/layout.jsx
@@ -12,7 +12,7 @@ export default function Layout() {
 				<motion.div
 					key={pathname}
 					initial={{ y: '50%', opacity: 0 }}
-					animate={{ y: '0%', opacity: 1 }}
+					animate={{ y: '0%', opacity: 1, transition: { type: 'spring', stiffness: 300, damping: 40 } }}
 					className='pt-20 px-4 text-neutral-900 min-h-screen'
 				>
 					<Outlet />
